9fff591d31cd5207b94ea17d386424537289455f
indunil-19 <induniludayangana.18@cse.mrt.ac.lk>
2021-11-10 18:37:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 
 
 app.use(function(req, res, next) {
+    console.log("ssdsdsd")
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
